711749d5c8856d8b6aecf14e485d4fea8bee15c5
Pushkar S <spushkar0071234@gmail.com>
2024-08-11 00:06:53 +0530

    testing new git

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -4,6 +4,9 @@ export default function ForgotPassword() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="font-bold text-3xl">Forgot Password</h1>
+      <p className="mt-2 text-sm text-gray-500">
+        Enter your email to reset your password
+      </p>
     </div>
   );
 }
